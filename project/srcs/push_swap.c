a0f1e638742821c0096e060d3d20666e93cad769
Sophie <socarboni@gmail.com>
2021-09-17 15:24:52 +0200

    norme, ok

--- project/srcs/push_swap.c
+++ project/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 18:54:29 by scarboni          #+#    #+#             */
-/*   Updated: 2021/09/17 15:09:21 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/09/17 15:24:31 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	rreverse(t_env *env, int ra_cnt, int rb_cnt, int (*fun)(t_env *, int))
 {
-	// printf("RA %d: RB %d:\n", ra_cnt, rb_cnt);
 	int	i;
 
 	i = 0;
@@ -35,151 +34,127 @@ void	rreverse(t_env *env, int ra_cnt, int rb_cnt, int (*fun)(t_env *, int))
 	}
 }
 
-void	sort_lowcase(t_env *env, int cnt, int flag, int (*fun)(t_env *, int))
+void	sort_b_up_to_2(t_env *env, int cnt, int (*fun)(t_env *, int))
 {
-	// printf("sort_lowcase %d:%d\n",  cnt, flag);
-	if (flag == 1 && cnt == 2)
+	if (cnt == 1)
 	{
-		if (get_value(env->a.head->content) > get_value(env->a.head->next->content))
-			fun(env, ACT_ID_S_ + ACT_ID__A);
+		fun(env, ACT_ID_P_ + ACT_ID__A);
+		return ;
 	}
-	if (flag == 0)
+	else if (cnt != 2)
+		return ;
+	if (get_value(env->b.head->content) > get_value(env->b.head->next->content))
 	{
-		if (cnt == 1)
-			fun(env, ACT_ID_P_ + ACT_ID__A);
-		else if (cnt == 2)
-		{
-			if (get_value(env->b.head->content) > get_value(env->b.head->next->content))
-			{
-				fun(env, ACT_ID_P_ + ACT_ID__A);
-				fun(env, ACT_ID_P_ + ACT_ID__A);
-			}
-			else
-			{
-				fun(env, ACT_ID_S_ + ACT_ID__B);
-				fun(env, ACT_ID_P_ + ACT_ID__A);
-				fun(env, ACT_ID_P_ + ACT_ID__A);
-			}
-		}
+		fun(env, ACT_ID_P_ + ACT_ID__A);
+		fun(env, ACT_ID_P_ + ACT_ID__A);
+	}
+	else
+	{
+		fun(env, ACT_ID_S_ + ACT_ID__B);
+		fun(env, ACT_ID_P_ + ACT_ID__A);
+		fun(env, ACT_ID_P_ + ACT_ID__A);
 	}
 }
+
+#define RA	0
+#define RB	1
+#define P	2
 
 void	sort_b_to_a(t_env *env, int cnt, int (*fun)(t_env *, int))
 {
-	// printf("sort_b_to_a\n");
-	int ra_cnt = 0;
-	int rb_cnt = 0;
-	int p_cnt = 0;
-	int pivots[2];
+	int	counters[3];
+	int	pivots[2];
 
+	ft_memset(counters, 0, sizeof(int) * 3);
 	if (cnt <= 2)
-		return (sort_lowcase(env, cnt, 0, fun));
+		return (sort_b_up_to_2(env, cnt, fun));
 	find_pivot(env, env->b, pivots, cnt);
-	// printf("PIVOTS %d:%d:%d\n", pivots[0], pivots[1], cnt);
 	while (cnt--)
 	{
-
-	// printf("PIVOTS %d:%d:%d\n", pivots[0], pivots[1], cnt);
 		if (get_value(env->b.head->content) < pivots[0])
-		{
-			rb_cnt += fun(env, ACT_ID_R_ + ACT_ID__B);
-		}
+			counters[RB] += fun(env, ACT_ID_R_ + ACT_ID__B);
 		else
 		{
-			p_cnt += fun(env, ACT_ID_P_ + ACT_ID__A);
+			counters[P] += fun(env, ACT_ID_P_ + ACT_ID__A);
 			if (get_value(env->a.head->content) < pivots[1])
-				ra_cnt += fun(env, ACT_ID_R_ + ACT_ID__A);
+				counters[RA] += fun(env, ACT_ID_R_ + ACT_ID__A);
 		}
 	}
-	// printf("p %d:ra %d:rb %d: cnt %d\n", p_cnt, ra_cnt, rb_cnt, cnt);
-	sort_a_to_b(env, p_cnt - ra_cnt, fun);
-	rreverse(env, ra_cnt, rb_cnt, fun);
-	sort_a_to_b(env, ra_cnt, fun);
-	sort_b_to_a(env, rb_cnt, fun);
-	// sort_a_to_b(env, p_cnt - ra_cnt, fun);
-	// rreverse(env, ra_cnt, rb_cnt, fun);
-	// sort_a_to_b(env, ra_cnt, fun);
-	// sort_b_to_a(env, env->b.total_item, fun);
-	// printf("END sort_b_to_a\n");
+	sort_a_to_b(env, counters[P] - counters[RA], fun);
+	rreverse(env, counters[RA], counters[RB], fun);
+	sort_a_to_b(env, counters[RA], fun);
+	sort_b_to_a(env, counters[RB], fun);
 }
+
+
+// void	sort_b_to_a(t_env *env, int cnt, int (*fun)(t_env *, int))
+// {
+// 	int	counters[3];
+// 	int	pivots[2];
+
+// 	ft_memset(counters, 0, sizeof(int) * 3);
+// 	if (cnt <= 2)
+// 		return (sort_b_up_to_2(env, cnt, fun));
+// 	find_pivot(env, env->b, pivots, cnt);
+// 	while (cnt--)
+// 	{
+// 		if (get_value(env->b.head->content) < pivots[0])
+// 			counters[RB] += fun(env, ACT_ID_R_ + ACT_ID__B);
+// 		else
+// 		{
+// 			counters[P] += fun(env, ACT_ID_P_ + ACT_ID__A);
+// 			if (get_value(env->a.head->content) < pivots[1])
+// 				counters[RA] += fun(env, ACT_ID_R_ + ACT_ID__A);
+// 		}
+// 	}
+// 	sort_a_to_b(env, counters[P] - counters[RA], fun);
+// 	rreverse(env, counters[RA], counters[RB], fun);
+// 	sort_a_to_b(env, counters[RA], fun);
+// 	sort_b_to_a(env, counters[RA], fun);
+// }
 
 void	sort_a_to_b(t_env *env, int cnt, int (*fun)(t_env *, int))
 {
-	// printf("sort_a_to_b\n");
-	int ra_cnt = 0;
-	int rb_cnt = 0;
-	int p_cnt = 0;
-	int pivots[2];
+	int	counters[3];
+	int	pivots[2];
 
+	ft_memset(counters, 0, sizeof(int) * 3);
 	if (cnt <= 2)
-		return (sort_lowcase(env, cnt, 1, fun));
+		return (sort_two(env, env->a, ACT_ID__A, fun));
 	if (check_order_until_i(env->a, cnt) == ORDERED)
 		return ;
 	find_pivot(env, env->a, pivots, cnt);
-	// printf("PIVOTS %d:%d:%d:%d\n", pivots[0], pivots[1], cnt, check_if_all_bigger(env->a, pivots[1]));
-	// int print = false;
-	// 		if( pivots[0] == 403 && pivots[1] == 406 && cnt == 4)
-	// 			print = true;
 	while (cnt && check_if_all_bigger_until_i(env->a, pivots[1], cnt) == MISC)
 	{
-
-		// if( print == true){
-			// printf("PIVOTS %d:%d:%d:%d\n", pivots[0], pivots[1], cnt, check_if_all_bigger(env->a, pivots[1]));
-
-		// 	printf("%d\n\n",get_value(env->a.head->content));}
-		if (get_value(env->a.head->content) >= pivots[1]){
-			// if( print == true)
-			// 	print_both(env);
-			ra_cnt += fun(env, ACT_ID_R_ + ACT_ID__A);
-		}
+		if (get_value(env->a.head->content) >= pivots[1])
+			counters[RA] += fun(env, ACT_ID_R_ + ACT_ID__A);
 		else
 		{
-			p_cnt += fun(env, ACT_ID_P_ + ACT_ID__B);
+			counters[P] += fun(env, ACT_ID_P_ + ACT_ID__B);
 			if (get_value(env->b.head->content) >= pivots[0])
-				rb_cnt += fun(env, ACT_ID_R_ + ACT_ID__B);
+				counters[RB] += fun(env, ACT_ID_R_ + ACT_ID__B);
 		}
 		cnt--;
 	}
-	// printf("p %d:ra %d:rb %d: cnt %d\n", p_cnt, ra_cnt, rb_cnt, cnt);
-
-	rreverse(env, ra_cnt, rb_cnt, fun);
-	ra_cnt += cnt;
-	sort_a_to_b(env, ra_cnt, fun);
-	sort_b_to_a(env, rb_cnt, fun);
-	sort_b_to_a(env, p_cnt - rb_cnt, fun);
-	// rreverse(env, ra_cnt, rb_cnt, fun);
-	// ra_cnt += cnt;
-	// sort_a_to_b(env, ra_cnt, fun);
-	// sort_b_to_a(env, env->b.total_item, fun);
-	// sort_b_to_a(env, p_cnt - rb_cnt, fun);
-	// printf("END sort_a_to_b\n");
+	rreverse(env, counters[RA], counters[RB], fun);
+	counters[RA] += cnt;
+	sort_a_to_b(env, counters[RA], fun);
+	sort_b_to_a(env, counters[RB], fun);
+	sort_b_to_a(env, counters[P] - counters[RB], fun);
 }
 
 void	push_swap(t_env *env)
 {
 	if (env->total_item < 2)
-		return;
-	else if (env->total_item == 2)
-	{
-		sort_two(env, env->a, ACT_ID__A, &start_action_ps);
 		return ;
-	}
-	else if (env->total_item == 3)
-	{
-		sort_three(env, env->a, ACT_ID__A, &start_action_ps);
-		return ;
-	}
-	else if (env->total_item == 4)
-	{
-		start_brute_force(env);
-		return ;
-	}
-	else if (env->total_item == 5)
-	{
-		sort_five(env, &start_action_ps);
-		print_both(env);
-		return ;
-	}
+	if (env->total_item == 2)
+		return (sort_two(env, env->a, ACT_ID__A, &start_action_ps));
+	if (env->total_item == 3)
+		return (sort_three(env, env->a, ACT_ID__A, &start_action_ps));
+	if (env->total_item == 4)
+		return (start_brute_force(env));
+	if (env->total_item == 5)
+		return (sort_five(env, &start_action_ps));
 	init_position_array(env);
 	sort_a_to_b(env, env->total_item, &start_action_for_optimization);
 	optimise_action_stack(env);
